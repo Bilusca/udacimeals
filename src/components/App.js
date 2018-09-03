81f92799bf9d2ca190de29c382a518502ae70c39
lucasactuar <lucas.vieira@actuar.group>
2018-09-03 17:58:29 -0300

    Adding React-Redux

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        
+      <div>
+        Hello World
       </div>
     );
   }
